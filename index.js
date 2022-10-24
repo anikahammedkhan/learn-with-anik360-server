db01482db647d4093383e06ad45f6dbf4c1ffcf8
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-10-24 23:08:27 +0600

    fakeData genareted

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const data = require('./data/courseData.json')
+
 app.get('/', (req, res) => {
     res.send('Welcome to Learn With Anik360 server!')
 })
+
+
+app.get('/courseData', (req, res) => {
+    res.send(data)
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
