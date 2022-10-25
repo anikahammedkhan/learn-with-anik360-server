70eab1a949ba0b9bb08a3008d42fb0a0833e85dc
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-10-26 00:35:22 +0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 const courseData = require('./data/courseData.json')
 
